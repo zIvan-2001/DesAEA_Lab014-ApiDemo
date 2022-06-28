7376600640abcf2898c4d3b3bce1bf61b174b94e
zIvan-2001 <ivan.antonio@tecsup.edu.pe>
2022-06-28 01:53:16 -0500

    Add project files.

---
+++ ApiDemo/Controllers/PersonsController.cs
@@ -0,0 +1,33 @@
+﻿using ApiDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiDemo.Controllers
+{
+    public class PersonsController : ApiController
+    {
+        public List<PersonResponse> Get()
+        {
+            var result = new List<PersonResponse>();
+            result.Add(new PersonResponse { FirstName = "Ivan", LastName = "Antonio" });
+            result.Add(new PersonResponse { FirstName = "Anthony", LastName = "Quito" });
+            result.Add(new PersonResponse { FirstName = "Elsin", LastName = "Vila" });
+            return result;
+        }
+
+        public PersonResponse Insert(PersonRequest request)
+        {
+            var result = new PersonResponse();
+
+            result.FirstName = request.FirstName;
+            result.LastName = request.LastName;
+
+            return result;
+
+        }
+
+    }
+}
---
+++ ApiDemo/Models/PersonResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiDemo.Models
+{
+    public class PersonResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
